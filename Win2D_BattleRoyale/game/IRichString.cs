88b45c48694d313a58c321764e68cbdf8766cd77
Joshua Baxter <baxter.josh@gmail.com>
2015-08-23 00:11:46 -0700

    Adding IRichString interface for RichListBox drawing

---
+++ Win2D_BattleRoyale/game/IRichString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win2D_BattleRoyale
+{
+    public interface IRichString
+    {
+        RichStringPart ToRichString();
+    }
+}
